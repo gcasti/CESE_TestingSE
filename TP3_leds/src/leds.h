51766c32f9ffa1ccfb1ce1334c3340c6fc64b2cd
gcasti <gcasti_17@yahoo.com.ar>
2022-06-01 09:43:09 -0300

    Comentarios
    Se adicionan comentarios en las funciones.
    Se ignoran archivos de generados durante la compilación

--- TP3_leds/src/leds.h
+++ TP3_leds/src/leds.h
@@ -3,18 +3,47 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-/**Documentar
-*/
+
+/**
+ * @brief Configura el módulo y apaga todos los leds
+ * 
+ * @param direction Dirección del puerto GPIO que controla los leds
+ */
 void LedsInit(uint16_t * direction);
 
+/**
+ * @brief Prende un led
+ * 
+ * @param led Número de led a prender (1 a 16)
+ */
 void LedTurnOn(uint8_t led);
 
+/**
+ * @brief Apaga un led
+ * 
+ * @param led Número de led a apagar (1 a 16)
+ */
 void LedTurnOff(uint8_t led);
 
+/**
+ * @brief Prende todos los leds
+ * 
+ */
 void LedTurnOnAll(void);
 
+/**
+ * @brief Apaga todos los leds
+ * 
+ */
 void LedTurnOffAll(void);
 
+/**
+ * @brief Obtiene el estado de un led específico
+ * 
+ * @param led Número de led a apagar (1 a 16)
+ * @return true : led prendido
+ * @return false : led apagado 
+ */
 bool getStateLed(uint8_t led);
       
 #endif
